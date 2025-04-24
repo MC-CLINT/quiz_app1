649b835d4d16952a607055449505b59bac46d0f7
MC-CLINT <ckvidam@st.ug.edu.gh>
2025-04-24 15:23:29 +0000

    set

---
+++ quiz_app/src/Components/Others/QuestionsBank.jsx
@@ -0,0 +1,25 @@
+export  const Questions = [
+    {
+        question: "What is the capital of France?",
+        options: ["Berlin", "Madrid", "Paris", "Rome"],
+        answer: 2,
+    },
+    {
+        question: "Which planet is known as the Red Planet?",
+        options: ["Earth", "Mars", "Jupiter", "Saturn"],
+        answer: 1,
+    },
+    {
+        question: "Who wrote 'To Kill a Mockingbird'?",
+        options: ["Harper Lee", "Mark Twain", "F. Scott Fitzgerald", "Ernest Hemingway"],
+        answer: 0,
+    },
+    {
+        question: "What is the largest ocean on Earth?",
+        options: ["Atlantic Ocean", "Indian Ocean", "Arctic Ocean", "Pacific Ocean"],
+        answer: 3,
+    },
+
+    
+    
+]
--- quiz_app/src/Components/Quiz/Quiz.jsx
+++ quiz_app/src/Components/Quiz/Quiz.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
+import {useState} from 'react'
+import { Questions } from '../Others/QuestionsBank';
 
 const Quiz = () => {
+  const [questions, setQuestions] = useState(0);
   return (
     <div>
 
+
       <h1>Quiz</h1>
-
+      <p>Welcome to the quiz! Answer the questions below:</p>
+      <h2>{Questions[questions].question}</h2>
+      <div className='options'>
+        {Questions[questions].options.map((option, index) => (
+          <button key={index} onClick={() => setQuestions(questions + 1)}>
+            {option}
+          </button>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Quiz
+export default Quiz;
